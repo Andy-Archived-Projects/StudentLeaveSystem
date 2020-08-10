c184d28a2ca10608d7eec6d7ccacedbe9292ccb5
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2020-08-10 12:03:24 +0800

    auto pack to exe

--- client/installer.py
+++ client/installer.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 import os
+bat_content = '''@echo off
+echo =======启动中请稍后 Starting up Please wait=======
+client.exe
+echo Press Enter to exit
+set /p a=
+'''
+folder_name = 'LeaveSystemClient'
 
 os.system('@echo off')
-os.system('rd /s /q client')
+os.system(f'rd /s /q {folder_name}')
 os.system('pyinstaller -F client.py')
 os.system('xcopy /e /y /q /i lang dist\lang')
 os.system('rd /s /q build')
 os.system('rd /s /q __pycache__')
 os.system('del client.spec')
-os.rename('dist', 'client')
-with open('client\\Start.bat', 'w') as f:
-    f.write('@echo off\necho ====启动中 请稍后====\nclient.exe\npause')
+os.rename('dist', folder_name)
+with open(f'{folder_name}\\Start.bat', 'w') as f:
+    f.write(bat_content)
